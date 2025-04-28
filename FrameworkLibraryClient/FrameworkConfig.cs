8a8cd49d44168620678c6eee8440948e7705e563
Alexander <alexander.neitzel@garmingo.com>
2025-04-28 16:08:12 +0200

    Added missing semicolon

--- FrameworkLibraryClient/FrameworkConfig.cs
+++ FrameworkLibraryClient/FrameworkConfig.cs
@@ -12,4 +12,5 @@ namespace FrameworkLibraryClient
         public string ESXEvent { get; set; }
         public string ExportResource { get; set; }
         public bool ShowDebugInfo { get; set; }
+    }
 }
